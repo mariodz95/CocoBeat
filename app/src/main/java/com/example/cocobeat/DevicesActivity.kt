fb3e65aa09739a79946b2a140c99d0416693d2d1
mariodz95 <mariodz95@gmail.com>
2021-01-29 19:50:21 +0100

    Change back arrow color

--- app/src/main/java/com/example/cocobeat/DevicesActivity.kt
+++ app/src/main/java/com/example/cocobeat/DevicesActivity.kt
@@ -2,13 +2,12 @@ package com.example.cocobeat
 
 import android.os.Bundle
 import android.view.View
-import android.view.Window
 import android.widget.Button
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 
 class DevicesActivity : AppCompatActivity() {
     private val deviceList = ArrayList<DeviceViewModel>()
@@ -21,17 +20,23 @@ class DevicesActivity : AppCompatActivity() {
             deviceList.add(  DeviceViewModel(R.drawable.ic_launcher_background,"Device 1", "xx/xx/XXXX"))
         }
 
-        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar()?.setCustomView(R.layout.toolbar_device_list_title);
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
+        supportActionBar?.setCustomView(R.layout.toolbar_device_list_title);
 
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
-
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            // back button pressed
+            onBackPressed()
+        }
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         recyclerView.adapter = DevicesAdapter(deviceList)
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
 
     fun openDeviceDialog(view: View) {
         val addDevice: Button = findViewById(R.id.button_add_device)
