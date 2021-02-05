85bd238cbf1c5843a6d707d8cbfbeae4e97f688a
mariodz95 <mariodz95@gmail.com>
2021-02-05 13:01:13 +0100

    Read device characteristic

--- app/src/main/java/com/example/cocobeat/fragment/DeviceListDialogFragment.kt
+++ app/src/main/java/com/example/cocobeat/fragment/DeviceListDialogFragment.kt
@@ -1,31 +1,21 @@
 package com.example.cocobeat.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.ablelib.comm.asyncComm
-import com.ablelib.exceptions.BluetoothStateException
-import com.ablelib.manager.AbleManager
-import com.ablelib.manager.pair
-import com.ablelib.models.AbleDevice
-import com.ablelib.models.AbleUUID
-import com.ablelib.storage.AbleDeviceStorage
-import com.example.cocobeat.model.DeviceDataModel
 import com.example.cocobeat.R
 import com.example.cocobeat.adapter.DevicesAdapter
 import com.example.cocobeat.databinding.ActivityDeviceDialogBinding
+import com.example.cocobeat.model.DeviceDataModel
+import com.example.cocobeat.repository.BluetoothService
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import java.util.*
 import kotlin.collections.ArrayList
 
-private val UUID_DEVICE_INFORMATION_SERVICE  = AbleUUID(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"))
-private val UUID_DEVICE_SERIAL_NUMBER_CHARACTERISTIC  = AbleUUID(UUID.fromString("0x100001-0000-1000-8000-00805f9b34fb"))
 
 class DeviceListDialogFragment : DialogFragment(), DevicesAdapter.OnItemClickListener {
     private val deviceList = ArrayList<DeviceDataModel>()
@@ -34,18 +24,18 @@ class DeviceListDialogFragment : DialogFragment(), DevicesAdapter.OnItemClickLis
 
 
     override  fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, saveInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?, saveInstanceState: Bundle?
     ): View? {
         _binding = ActivityDeviceDialogBinding.inflate(inflater, container, false)
         val view = binding.root
         binding.recyclerViewDialog.layoutManager = LinearLayoutManager(this.context)
 
         deviceList.add(
-            DeviceDataModel(
-                R.drawable.ic_launcher_background,
-                "Movesense Wearable",
-                null
-            )
+                DeviceDataModel(
+                        R.drawable.ic_launcher_background,
+                        "Movesense Wearable",
+                        null
+                )
         )
         deviceList.add(DeviceDataModel(R.drawable.ic_launcher_background, "Accu-Chek", null))
 
@@ -74,87 +64,17 @@ class DeviceListDialogFragment : DialogFragment(), DevicesAdapter.OnItemClickLis
     override fun onItemClick(position: Int) {
         val clickedItem = deviceList[position]
         GlobalScope.async  {
-            scanForNearbyDevices(clickedItem.deviceName)
+            connectToDevice(clickedItem.deviceName)
         }
     }
 
-    suspend fun scanForNearbyDevices(deviceName: String) {
+    private suspend fun connectToDevice(deviceName: String) {
+        val bluetoothService = BluetoothService()
         val value = GlobalScope.async {
-            try {
-                val devices = AbleManager.shared.scan()
-                for (device in devices) {
-                    AbleDeviceStorage.default.add(
-                        AbleDevice(
-                            device.name,
-                            true,
-                            true,
-                            true,
-                            device.address
-                        )
-                    )
-                }
-            } catch (e: BluetoothStateException) {
-                // handle the exception
-            }
+            bluetoothService.scanForNearbyDevices(deviceName)
         }
         value.await()
-        pairWithDevice(deviceName)
-        val myDevice = AbleDeviceStorage.default.findByName(deviceName)
-        Log.v("test", "myDevice $myDevice")
-/*
-        val comm = myDevice.comm
-*/
-/*
-        Log.v("test", "comm $comm")
-        val connect = comm.connect()
-        Log.v("test", "connect $connect")
-        val UUID =  AbleUUID(UUID.randomUUID().toString())
-
-        try {
-            val characteristic = comm.discoverServices().first { service -> service.uuid == UUID_DEVICE_INFORMATION_SERVICE }
-
-
-        }catch (e: java.lang.Exception){
-            Log.v("test", "characteristic error $e")
-        }
-*/
-
-
-
-            val comm = myDevice.asyncComm.onConnectionStateChanged { newState, status ->
-                        // respond to connection state changes - connections, disconnects, etc
-                         Log.v("test", status.toString())
-                    }
-                    .onServicesDiscovered { services ->
-                        // here's where the services will appear once you trigger discoverServices
-                        Log.v("test", "onServicesDiscovered $services")
-                    }
-                    .onCharacteristicRead { characteristic ->
-                        // called after comm.readCharacteristic
-                        Log.v("test", "onCharacteristicRead $characteristic")
-                    }
-                    .onCharacteristicChanged { characteristic ->
-                        // called when a characteristic for which setNotifyValue was called changes its value
-                        Log.v("test", "onCharacteristicChanged")
-                        Log.v("test", "onCharacteristicChanged test $characteristic")
-                    }
-                    .onDescriptorRead { descriptor ->
-                        // called after comm.readDescriptor
-                        Log.v("test", "onDescriptorRead")
-                    }
-                    .onError { error ->
-                        Log.v("test", "Error")
-
-                    }
-                    .connect()
-    }
-
-    suspend fun pairWithDevice(deviceName: String) {
-        val myDevice = AbleDeviceStorage.default.findByName(deviceName)
-        try {
-            val pairedDevice = myDevice.pair()
-        } catch (e: Exception) {
-            // handle the exception
-        }
+        bluetoothService.pairWithDevice(deviceName)
+        bluetoothService.readCharacteristic(deviceName)
     }
 }
---
+++ app/src/main/java/com/example/cocobeat/repository/BluetoothService.kt
@@ -0,0 +1,90 @@
+package com.example.cocobeat.repository
+
+
+import android.util.Log
+import com.ablelib.comm.comm
+import com.ablelib.exceptions.BluetoothStateException
+import com.ablelib.manager.AbleManager
+import com.ablelib.manager.pair
+import com.ablelib.models.AbleDevice
+import com.ablelib.models.AbleUUID
+import com.ablelib.storage.AbleDeviceStorage
+import com.example.cocobeat.util.ConvertBytesToInt
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import java.util.*
+
+class BluetoothService {
+     suspend fun scanForNearbyDevices(deviceName: String) {
+
+        val value = GlobalScope.async {
+            try {
+                val devices = AbleManager.shared.scan()
+                for (device in devices) {
+                    AbleDeviceStorage.default.add(
+                            AbleDevice(
+                                    device.name,
+                                    true,
+                                    true,
+                                    true,
+                                    device.address
+                            )
+                    )
+                }
+            } catch (e: BluetoothStateException) {
+                // handle the exception
+            }
+        }
+    }
+
+     suspend fun pairWithDevice(deviceName: String) {
+        val myDevice = AbleDeviceStorage.default.findByName(deviceName)
+        try {
+            val pairedDevice = myDevice.pair()
+        } catch (e: Exception) {
+            // handle the exception
+        }
+    }
+
+    suspend fun readCharacteristic(deviceName: String){
+
+        val myDevice = AbleDeviceStorage.default.findByName(deviceName)
+        Log.v("test", "myDevice $myDevice")
+        val comm = myDevice.comm
+        Log.v("test", "comm $comm")
+        val connect = comm.connect()
+        Log.v("test", "connect $connect")
+
+        try {
+            val characteristicModelNumber = comm.discoverServices().first { service -> service.uuid == AbleUUID("0000180a-0000-1000-8000-00805f9b34fb") }
+                    .getCharacteristic(AbleUUID("00002a24-0000-1000-8000-00805f9b34fb"))
+            val dataModelNumber = comm.readCharacteristic(characteristicModelNumber).value
+
+            val characteristicSerialNumber = comm.discoverServices().first { service -> service.uuid == AbleUUID("0000180a-0000-1000-8000-00805f9b34fb") }
+                    .getCharacteristic(AbleUUID("00002a25-0000-1000-8000-00805f9b34fb"))
+            val dataSerialNumber = comm.readCharacteristic(characteristicSerialNumber).value
+
+            val deviceModel = String(dataSerialNumber) + String(dataModelNumber)
+            Log.v("test", "deviceModel ${deviceModel}")
+
+            val characteristicDateTime = comm.discoverServices().first { service -> service.uuid == AbleUUID("00001808-0000-1000-8000-00805f9b34fb") }
+                    .getCharacteristic(AbleUUID("00002a08-0000-1000-8000-00805f9b34fb"))
+            val dateTimeBytes= comm.readCharacteristic(characteristicDateTime).value
+
+            val convert = ConvertBytesToInt()
+            val year: Int = convert.intFromTwoBytes(dateTimeBytes[0], dateTimeBytes[1])
+            val month: Int = convert.intFromOneByte(dateTimeBytes[2])
+            val day: Int = convert.intFromOneByte(dateTimeBytes[3])
+            val hours: Int = convert.intFromOneByte(dateTimeBytes[4])
+            val minutes: Int = convert.intFromOneByte(dateTimeBytes[5])
+            val seconds: Int = convert.intFromOneByte(dateTimeBytes[6])
+
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month - 1, day, hours, minutes, seconds);
+            Log.v("test", "date ${calendar.time}")
+        }catch (e: java.lang.Exception)
+        {
+        }
+    }
+
+}
